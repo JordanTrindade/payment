a0e90cafe61259816346566e4a19865bf73205f0
JordanTrindade <jordandimst@gmail.com>
2025-01-29 20:59:39 -0300

    Create AccountRepository.java

---
+++ src/main/java/com/api/pay/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.api.pay.repository;
+
+import com.api.pay.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
