b7e54af07f4599b254b4effa7327c58dda563dd0
JordanTrindade <jordandimst@gmail.com>
2025-02-04 18:35:49 -0300

    Update PayApplication.java

--- src/main/java/com/api/pay/PayApplication.java
+++ src/main/java/com/api/pay/PayApplication.java
@@ -2,8 +2,10 @@ package com.api.pay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("dev")
 public class PayApplication {
 
 	public static void main(String[] args) {
