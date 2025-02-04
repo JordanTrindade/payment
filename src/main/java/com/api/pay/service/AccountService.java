3ebe2dc455572c0039630dbcf15da1d84089804a
JordanTrindade <jordandimst@gmail.com>
2025-02-04 18:36:42 -0300

    Update AccountService.java

--- src/main/java/com/api/pay/service/AccountService.java
+++ src/main/java/com/api/pay/service/AccountService.java
@@ -1,14 +1,25 @@
 package com.api.pay.service;
 
+import com.api.pay.domain.Account;
+import com.api.pay.domain.dto.AccountDTO;
 import com.api.pay.repository.AccountRepository;
+import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-
+@Service
 public class AccountService {
     private final AccountRepository accountRepository;
 
     public AccountService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
+    }
+
+
+    public Account createAccount(AccountDTO accountDTO){
+        Account account = accountRepository.findBycpfCnpjOrEmail(accountDTO.cpfCnpj(), accountDTO.email());
+        if(account != null){
+            System.out.println("Account already exists");
+        }
+        return accountRepository.save(accountDTO.toAccount());
     }
     
 }
