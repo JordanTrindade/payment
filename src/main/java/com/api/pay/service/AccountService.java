3fe8f121c62736bd9ca8c0980acb31e1212bc644
JordanTrindade <jordandimst@gmail.com>
2025-02-05 16:52:23 -0300

    Update AccountService.java

--- src/main/java/com/api/pay/service/AccountService.java
+++ src/main/java/com/api/pay/service/AccountService.java
@@ -2,6 +2,7 @@ package com.api.pay.service;
 
 import com.api.pay.domain.Account;
 import com.api.pay.domain.dto.AccountDTO;
+import com.api.pay.exceptions.AccountDataAlreadyExistsException;
 import com.api.pay.repository.AccountRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +16,9 @@ public class AccountService {
 
 
     public Account createAccount(AccountDTO accountDTO){
-        Account account = accountRepository.findBycpfCnpjOrEmail(accountDTO.cpfCnpj(), accountDTO.email());
+        Account account = accountRepository.findByCpfCnpjOrEmail(accountDTO.cpfCnpj(), accountDTO.email());
         if(account != null){
-            System.out.println("Account already exists");
+            throw new AccountDataAlreadyExistsException("Email or Cpf/Cnpj already exists");
         }
         return accountRepository.save(accountDTO.toAccount());
     }
