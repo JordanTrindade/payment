eb074e20d049b9059c46a77a62c016b33d3794fb
JordanTrindade <jordandimst@gmail.com>
2025-02-05 16:52:25 -0300

    Update AccountRepository.java

--- src/main/java/com/api/pay/repository/AccountRepository.java
+++ src/main/java/com/api/pay/repository/AccountRepository.java
@@ -2,9 +2,19 @@ package com.api.pay.repository;
 
 import com.api.pay.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Account findBycpfCnpjOrEmail(String cpfCnpj, String email);
+    /*
+    @Query(
+            "SELECT ac FROM Account ac WHERE ac.cpfCnpj = :CPF_CNPJ or ac.email = :EMAIL"
+    )*/
+
+//    Account findByCpfCnpjOrEmail(@Param("CPF_CNPJ") String cpfCnpj, @Param("EMAIL") String email);
+
+    Account findByCpfCnpjOrEmail(String cpfCnpj, String email);
+
 }
