2b846e895d13f8910d1f7470331f1f0cf069312f
JordanTrindade <jordandimst@gmail.com>
2025-02-07 23:05:19 -0300

    Create AuthorizationClient.java

---
+++ src/main/java/com/api/pay/client/AuthorizationClient.java
@@ -0,0 +1,14 @@
+package com.api.pay.client;
+
+import com.api.pay.domain.dto.ResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(
+        url = "${client.authorization.service.url}"
+)
+public interface AuthorizationClient {
+    @GetMapping
+    ResponseEntity<ResponseDto> isAuthorized();
+}
