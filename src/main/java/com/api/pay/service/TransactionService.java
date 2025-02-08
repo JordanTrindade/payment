540a14eea104f2b796bac84b9cc4963cd713ef44
JordanTrindade <jordandimst@gmail.com>
2025-02-08 18:14:40 -0300

    Update TransactionService.java

--- src/main/java/com/api/pay/service/TransactionService.java
+++ src/main/java/com/api/pay/service/TransactionService.java
@@ -3,39 +3,44 @@ package com.api.pay.service;
 import com.api.pay.domain.Account;
 import com.api.pay.domain.Transaction;
 import com.api.pay.domain.dto.TransactionDto;
+import com.api.pay.exceptions.AccountWalletNotfoundException;
+import com.api.pay.exceptions.HasNotSufficientBalance;
+import com.api.pay.exceptions.PaymentExceptions;
+import com.api.pay.exceptions.TransactionIsNotAllowedForAccoutType;
 import com.api.pay.repository.AccountRepository;
+import com.api.pay.repository.TransactionRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class TransactionService {
     private final AccountRepository accountRepository;
     private final AuthorizationClientService authorizationClientService;
     private final NotificationClientService notificationClientService;
+    private final TransactionRepository transactionRepository;
 
-    public TransactionService(AccountRepository accountRepository, AuthorizationClientService authorizationClientService, NotificationClientService notificationClientService) {
+    public TransactionService(AccountRepository accountRepository, AuthorizationClientService authorizationClientService, NotificationClientService notificationClientService, TransactionRepository transactionRepository) {
         this.accountRepository = accountRepository;
         this.authorizationClientService = authorizationClientService;
         this.notificationClientService = notificationClientService;
+        this.transactionRepository = transactionRepository;
     }
 
-    //receber o pedido
-        //validar as carteiras (saldo e tipo)
-            //validar autorizador
-    //realizar transferencia
-
+    @Transactional
     public Transaction validTransfer(TransactionDto transactionDto){
-        Account sender =  accountRepository.findById(transactionDto.senderId()).orElse(null);
-        Account reciever =  accountRepository.findById(transactionDto.recieverId()).orElse(null);
+        Account sender =  accountRepository.findById(transactionDto.senderId()).orElseThrow(() -> new AccountWalletNotfoundException("Account not found, 404"));
+        Account reciever =  accountRepository.findById(transactionDto.recieverId()).orElseThrow(() -> new AccountWalletNotfoundException("Account not found, 404"));
         BigDecimal value = transactionDto.value();
 
         //valida se o sender pode realizar transferencia
-        if (!sender.isMerchantAccount())System.out.println("Account not merchant");;
+        if (sender.isMerchantAccount()) throw new TransactionIsNotAllowedForAccoutType("Account not allowed",sender);
         //valida saldo da carteira
-        if (!sender.hasSufficientBalance(value)) System.out.println("Account have not balance");;
-
-        if(!authorizationClientService.isAuthorized())System.out.println("Authorized client is out");
+        if (sender.hasNotSufficientBalance(value)) throw new HasNotSufficientBalance("Balance insuficient",value);
+        //verica servico autorizador
+        if(!authorizationClientService.isAuthorized()) throw new PaymentExceptions.transactionNotAuthorized();
 
         sender.debit(value);
         reciever.credit(value);
@@ -43,9 +48,12 @@ public class TransactionService {
         accountRepository.save(sender);
         accountRepository.save(reciever);
 
-        return new Transaction(sender,reciever, value);
+        Transaction transaction = new Transaction(sender,reciever, value);
+        transactionRepository.save(transaction);
+
+        //thread para envio da noticacao async para nao impactar o fluxo caso algo aconteca com o servico de notificacao
+        CompletableFuture.runAsync(()-> notificationClientService.sendNotification(transaction));
+
+        return transaction;
     }
-
-
-
 }
