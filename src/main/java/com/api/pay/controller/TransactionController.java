2f7f20be59f6511ab77172b0b6ee95a95f7c7313
JordanTrindade <jordandimst@gmail.com>
2025-02-08 13:34:20 -0300

    Update TransactionController.java

--- src/main/java/com/api/pay/controller/TransactionController.java
+++ src/main/java/com/api/pay/controller/TransactionController.java
@@ -1,11 +1,30 @@
 package com.api.pay.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api.pay.domain.Transaction;
+import com.api.pay.domain.dto.TransactionDto;
+import com.api.pay.service.TransactionService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "transaction")
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    ResponseEntity<Transaction> transfer(@RequestBody @Valid TransactionDto transactionDto){
+        Transaction transaction = transactionService.validTransfer(transactionDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(transaction);
+    }
 
 
 }
