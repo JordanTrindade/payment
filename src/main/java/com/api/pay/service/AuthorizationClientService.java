17870948b94c388dc9009d1ff6e4c5a733b081cc
JordanTrindade <jordandimst@gmail.com>
2025-02-07 23:05:39 -0300

    Create AuthorizationClientService.java

---
+++ src/main/java/com/api/pay/service/AuthorizationClientService.java
@@ -0,0 +1,24 @@
+package com.api.pay.service;
+
+import com.api.pay.client.AuthorizationClient;
+import com.api.pay.domain.Transaction;
+import com.api.pay.exceptions.PaymentExceptions;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationClientService {
+    private final AuthorizationClient authorizationClient;
+
+    public AuthorizationClientService(AuthorizationClient authorizationClient) {
+        this.authorizationClient = authorizationClient;
+    }
+
+    private Boolean isAuthorized(Transaction transaction){
+        var response = authorizationClient.isAuthorized();
+        if(response.getStatusCode().isError()){
+            throw new PaymentExceptions();
+        }
+        return response.getBody().success();
+    }
+
+}
