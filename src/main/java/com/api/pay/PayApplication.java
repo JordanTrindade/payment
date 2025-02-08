9d5d357006542c9f8c4105194bb779903b5e4a27
JordanTrindade <jordandimst@gmail.com>
2025-02-07 23:05:57 -0300

    Update PayApplication.java

--- src/main/java/com/api/pay/PayApplication.java
+++ src/main/java/com/api/pay/PayApplication.java
@@ -2,10 +2,12 @@ package com.api.pay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @Profile("dev")
+@EnableFeignClients
 public class PayApplication {
 
 	public static void main(String[] args) {
