addd83dc5583ee33c47830e9a9ba4bbd08c126ad
JordanTrindade <jordandimst@gmail.com>
2025-02-07 23:05:07 -0300

    Create NotificationClientService.java

---
+++ src/main/java/com/api/pay/service/NotificationClientService.java
@@ -0,0 +1,30 @@
+package com.api.pay.service;
+
+import com.api.pay.client.NoticationClient;
+import com.api.pay.domain.Transaction;
+import com.api.pay.exceptions.PaymentExceptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+
+public class NotificationClientService {
+
+    private final NoticationClient noticationClient;
+    private final Logger logger = LoggerFactory.getLogger(NotificationClientService.class);
+
+    public NotificationClientService(NoticationClient noticationClient) {
+        this.noticationClient = noticationClient;
+    }
+
+    public void SendNotification(Transaction transaction){
+        try {
+            logger.info("Sending notification");
+            ResponseEntity<Void> res = noticationClient.sendNotification(transaction);
+            if(res.getStatusCode().isError()){
+                logger.error("Error while sending notification");
+            }
+        }catch (PaymentExceptions paymentExceptions){
+            logger.error("Error while sending notification");
+        }
+    }
+}
